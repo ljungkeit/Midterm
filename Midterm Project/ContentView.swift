6a2575327aacb38899358c5c7381bcb5480b6d26
Luca Jungkeit <ljungkeit@vt.edu>
2026-03-25 14:54:18 -0400

    Initial Commit

---
+++ Midterm Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Midterm Project
+//
+//  Created by Luca Thomas Jungkeit on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Midterm Project/Midterm_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midterm_ProjectApp.swift
+//  Midterm Project
+//
+//  Created by Luca Thomas Jungkeit on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Midterm_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
